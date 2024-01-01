f5b57452c03f422423d8998f00fcf61e6f656909
Juliusmwash <cutejuliusmwash@gmail.com>
2024-01-01 21:31:13 +0300

    modify the permanent cookie session duration to one day

---
+++ find_and_comment.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+
+def comment_print_lines(filename, text_to_look_for):
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+        with open(filename, 'w') as file:
+            for line in lines:
+                if line.lstrip().startswith("print("):
+                    file.write(f'# {line}')
+                else:
+                    file.write(line)
+
+        print(f'Successfully commented lines starting with "print({text_to_look_for}" in {filename}.')
+    except FileNotFoundError:
+        print(f'Error: File "{filename}" not found.')
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <filename> <text_to_look_for>")
+    else:
+        filename = sys.argv[1]
+        text_to_look_for = sys.argv[2]
+        comment_print_lines(filename, text_to_look_for)
+
